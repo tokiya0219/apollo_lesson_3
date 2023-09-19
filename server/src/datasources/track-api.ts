0dad1609a025e05a7d9cf7e3dafa36ed5200ee7e
tokiya0219 <53889976+tokiya0219@users.noreply.github.com>
2023-09-20 00:06:29 +0900

    apollo_lesson_3

--- server/src/datasources/track-api.ts
+++ server/src/datasources/track-api.ts
@@ -1,5 +1,5 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { TrackModel, AuthorModel } from "../models"
+import { AuthorModel, TrackModel, ModuleModel } from "../models";
 
 export class TrackAPI extends RESTDataSource {
   baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
@@ -8,7 +8,15 @@ export class TrackAPI extends RESTDataSource {
     return this.get<TrackModel[]>("tracks");
   }
 
+  getTrack(trackId: string) {
+    return this.get<TrackModel>(`track/${trackId}`);
+  }
+
   getAuthor(authorId: string) {
     return this.get<AuthorModel>(`author/${authorId}`);
+  }
+
+  getTrackModules(trackId: string) {
+    return this.get<ModuleModel[]>(`track/${trackId}/modules`);
   }
 }
